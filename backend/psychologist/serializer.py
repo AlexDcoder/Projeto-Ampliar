c86328a8a203e307e2d3bb474bc59f8ad022a491
Alex <alexandrepinto586@gmail.com>
2025-06-21 10:04:37 -0300

    fix: Correção de campos e serializer do psicólogo

--- backend/psychologist/serializer.py
+++ backend/psychologist/serializer.py
@@ -6,3 +6,11 @@ class PsychologistSerializer(ModelSerializer):
         model = Psychologist
         fields = '__all__'
         read_only_fields = ['id']
+    
+    def validate_crp(self, value):
+        if self.instance:
+            if Psychologist.objects.exclude(id=self.instance.id).filter(crp=value).exists():
+                raise ValidationError("This CRP is already registered.")
+        if Psychologist.objects.filter(crp=value).exists():
+            raise ValidationError("This CRP is already registered.")
+        return value
